39abe453f9f75f690731c8727e96ba5641b0f856
DanielFloreTrejo <174523106+DanielFloreTrejo@users.noreply.github.com>
2026-05-14 20:34:30 -0300

    Se agrego la logica de las interfaz de ProductService y ProductServiceImpl

--- src/main/java/com/dflorestrejo/pharmacysystem/service/ProductService.java
+++ src/main/java/com/dflorestrejo/pharmacysystem/service/ProductService.java
@@ -1,4 +1,19 @@
 package com.dflorestrejo.pharmacysystem.service;
 
+import com.dflorestrejo.pharmacysystem.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductService {
+
+    Product add(Product product);
+
+    List<Product> findAll();
+
+    Optional<Product> findById(int id);
+
+    void update(Product product);
+
+    void delete(int id);
 }
--- src/main/java/com/dflorestrejo/pharmacysystem/service/ProductServiceImpl.java
+++ src/main/java/com/dflorestrejo/pharmacysystem/service/ProductServiceImpl.java
@@ -1,4 +1,41 @@
 package com.dflorestrejo.pharmacysystem.service;
 
+import com.dflorestrejo.pharmacysystem.entity.Product;
+import com.dflorestrejo.pharmacysystem.repository.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product add(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Product> findById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(Product product) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
